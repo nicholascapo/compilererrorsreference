8cb45e1412ea54d4bd11b7c97d5b50cb6888d2e2
Nicholas Capo <Nicholas Capo'@localhost>
2011-08-23 08:02:37 -0500

    Refactored generateCompilerMessages.py to allow for plaintext and LaTeX output

--- generateCompilerMessages.py
+++ generateCompilerMessages.py
@@ -7,26 +7,25 @@ import os
 
 
 ###############################################
-
+###############################################
 ## USER CONFIGURATION OPTIONS
 
 # compiler command to use, source code filename will be appended
 compiler = 'g++ -Wall'
 
-# the language is use (This needs to correspond to a language supported by the LaTeX listings package
+# the language is use (This needs to correspond to a language supported by the LaTeX listings package)
 lang = 'C++'
 
 # filename extension used for temporary source files
 source_filename_extension = '.cpp'
 
-## 
-
+# whether the output should be in LaTeX or plain text (True / False)
+output_latex = True
+###############################################
 ###############################################
 
-special_chars = ['#', '%', '{', '}']
 
 ###############################################
-
 def main():
         if len(sys.argv) < 2:
                 usage()
@@ -35,16 +34,20 @@ def main():
 		
 		check_no_error_compile()
 		
-		latex_header()
-		include_original_source(source_lines, sys.argv[1])
+		if output_latex:
+			processor = latex_processor()
+		else:
+			processor = plaintext_processor()
+		
+		processor.header()
+		processor.include_file(source_lines, sys.argv[1])
 		for i in range(0, len(source_lines)):
 			if source_lines[i] == '\n': continue
-			process_code(source_lines[i], source_lines[:i] + source_lines[i+1:], i)
+			process_code(source_lines[i], source_lines[:i] + source_lines[i+1:], i, processor)
 
-		latex_footer()
+		processor.footer()
 
 ###############################################
-
 def check_no_error_compile():
 	cmd = compiler + ' ' + sys.argv[1]
 	p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
@@ -56,17 +59,6 @@ def check_no_error_compile():
 		sys.exit(1)
 
 ###############################################
-
-def include_original_source(source_lines, source_filename):
-	latex_command('begin', 'center')
-	latex_command('large', 'Original Source')
-	latex_command('end', 'center')
-	latex_command('lstset', 'numbers=left')
-	latex_command('lstinputlisting[language=%s]' % lang, source_filename)
-	print('\\newpage')
-
-###############################################
-
 def get_lines(filename):
 	res = []
 	try:
@@ -80,8 +72,8 @@ def get_lines(filename):
 	return res
 
 ###############################################
-
-def process_code(commented_line, source, line_number):
+def process_code(commented_line, source, line_number, processor):
+	
 	tempfilename = 'tempfile' + source_filename_extension
 	f = open(tempfilename, 'w')
 	for l in source:
@@ -93,27 +85,44 @@ def process_code(commented_line, source, line_number):
 	p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
 	stdout, stderr = p.communicate()
 	
-	section_name = commented_line.strip() + ' [Line %d]' % int(line_number + 1)
-	latex_command('section', section_name)
+	processor.section(commented_line.strip(), int(line_number + 1))
 	
 	if stderr:
-		latex_command('begin', 'verbatim')
-		print(stderr)
-		latex_command('end', 'verbatim')
-		print('\n\n')
+		processor.compile_error(stderr)
 	else:
-		print('No Warnings or Errors')
-		print('\n\n')
+		processor.no_problem()
 
 ###############################################
-
 def usage():
         print('Usage: generateCompilerMessages.py source.cpp', file=sys.stderr)
 
 ###############################################
+###############################################
+###############################################
+class latex_processor():
 
-def latex_header():
-	res = '''
+	special_chars = ['#', '%', '{', '}']
+
+	def latex_command(self, command, text):
+		text = text.strip()
+		for c in self.special_chars:
+			text = text.replace(c, '\\' + c)
+		res = '\\%s{%s}' % (command, text)
+		print(res)
+
+
+	###############################################
+	def include_file(self, source_lines, source_filename):
+		self.latex_command('begin', 'center')
+		self.latex_command('large', 'Original Source')
+		self.latex_command('end', 'center')
+		self.latex_command('lstset', 'numbers=left')
+		self.latex_command('lstinputlisting[language=%s]' % lang, source_filename)
+		print('\\newpage')	
+
+	###############################################
+	def header(self, ):
+		res = '''
 \\documentclass{article}
 
 \\usepackage[pdftex, pdfusetitle, colorlinks, urlcolor=blue,]{hyperref}
@@ -126,25 +135,64 @@ def latex_header():
 \\maketitle
 \\tableofcontents
 \\newpage
-		'''
-	print(res % compiler)
+'''
+		print(res % compiler)
 
-###############################################
+	###############################################
+	def footer(self, ):
+		res = '\end{document}'
+		print(res)
+
+	###############################################
+	def compile_error(self, text):
+		self.latex_command('begin', 'verbatim')
+		print(text)
+		self.latex_command('end', 'verbatim')
+		print('\n')
+
+	###############################################
+	def no_problem(self, ):
+		print('No Warnings or Errors')
 	
-def latex_footer():
-	res = '\end{document}'
-	print(res)
+	###############################################
+	def section(self, name, line_number):
+		section_name = '%s [Line %d]' % (name, line_number)
+		self.latex_command('section', section_name)
 
 ###############################################
-
-def latex_command(command, text):
-	text = text.strip()
-	for c in special_chars:
-		text = text.replace(c, '\\' + c)
-	res = '\\%s{%s}' % (command, text)
-        print(res)
-
 ###############################################
+###############################################
+
+class plaintext_processor():
+
+	###############################################
+	def include_file(self, source_lines, source_filename):
+		print('Original Source:')
+		for l in source_lines:
+			print(l)
+		print('\n')	
+
+	###############################################
+	def header(self, ):
+		res = 'Iterative Line Removal: Compiler Output Messages using %s\n\n'
+		print(res % compiler)
+
+	###############################################
+	def footer(self, ):
+		pass
+
+	###############################################
+	def compile_error(self, text):
+		print(text)
+		
+
+	###############################################
+	def no_problem(self, ):
+		print('No Warnings or Errors')
+	
+	###############################################
+	def section(self, name, line_number):
+		print('Line %d: \t %s : ' % (line_number, name))
 
 if __name__ == '__main__': main()
 
