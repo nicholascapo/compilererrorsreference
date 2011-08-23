7dd7d9a9f5dda494036f2953f1526a7703c6eefa
'Nicholas Capo' <'Nicholas Capo'@localhost>
2011-08-23 08:10:32 -0500

    Added Copyright Notice and TODO list to generateCompilerMessages.py

--- generateCompilerMessages.py
+++ generateCompilerMessages.py
@@ -1,5 +1,31 @@
 #!/usr/bin/env python
 
+###
+# Name: Nicholas Capo
+
+# Purpose: Generates a listing of compiler error messages by removing each line of source code one at a time
+#          This is to be a reference for students as they learn the meaning of compiler messages
+# 
+# Copyright (C) 2011 Nicholas Capo
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 
+
+# TODO 
+# add ArgParse support
+# 
+
+
 from __future__ import print_function
 from subprocess import Popen, PIPE
 import sys
